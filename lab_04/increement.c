a20126129dec9647788bc0f97f3ebea1ba0c5a4b
shreyapatidar006-gif <shreyapatidar006@gmail.com>
2026-09-13 21:10:21 +0530

    increment_operators

---
+++ lab_04/increement.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a=3, b= a++; //post increment
+    printf("%d\n%d\n",a,b);
+    int c=6,d=++a; //pre-increment
+    printf("%d\n%d",c,d);
+    
+    return 0;
+
+}
