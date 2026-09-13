d50d2939ab0c8ae361bd571768638450760d6210
shreyapatidar006-gif <shreyapatidar006@gmail.com>
2026-09-13 21:27:00 +0530

    Update increment.c

--- lab_04/increment.c
+++ lab_04/increment.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main(){
+    printf("post increment:\n");
     int a=3, b= a++; //post increment
     printf("%d\n%d\n",a,b);
-    int c=6,d=++a; //pre-increment
+    printf("pre-increment:\n");
+    int c=6,d=++c; //pre-increment
     printf("%d\n%d",c,d);
     
     return 0;
